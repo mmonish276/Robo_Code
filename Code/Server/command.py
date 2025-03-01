f4244bca2e46efdf9213ebd46962ba623ca900a1
mmonish276 <mmonish276@gmail.com>
2025-03-01 15:28:23 -0600

    v4

--- Code/Server/command.py
+++ Code/Server/command.py
@@ -98,12 +98,12 @@ if __name__ == '__main__':
         while True:
             if (abs(center_error) > center_threshold):
                 if center_error > 0:
-                    Drive(700, -700)
+                    Drive(800, -800)
                     center_error = center_error - 1
                     print(center_error)
                     #check_error()
                 else:
-                    Drive(-700, 700)
+                    Drive(-800, 800)
                     #check_error()
             DropArm()
             PinchIn()

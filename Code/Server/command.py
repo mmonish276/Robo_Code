513c662e7a27cdd2bcf6b7796186511dd6af82f0
jjyuen2 <jjyuen2@illinois.edu>
2025-03-01 10:28:20 -0600

    minor fix

--- Code/Server/command.py
+++ Code/Server/command.py
@@ -23,7 +23,7 @@ def Pinch():
     import time                        # Import the time module for sleep functionality
     print('Pinching Servo')  # Print a start message
     servo = Servo()                    # Initialize the Servo instance
-    for i in range(90, 115, 1):     #(90, 140, 1)
+    for i in range(90, 140, 1):     #(90, 140, 1)
         servo.setServoAngle('0', i)  # Set servo 0 to angle i
         time.sleep(0.01)           # Wait for 0.01 seconds
     
@@ -33,7 +33,7 @@ def DropArm():
     print('Drop Arm')  # Print a start message
     servo = Servo()                    # Initialize the Servo instance
     
-    for i in range(140, 115, -1): #(140, 90, -1)
+    for i in range(140, 90, -1): #(140, 90, -1)
         print('i:', i)
         servo.setServoAngle('1', i)  # Set servo 1 to angle i
         time.sleep(0.01)           # Wait for 0.01 seconds
@@ -44,8 +44,8 @@ def StopAll():
     servo = Servo()
     drive = tankMotor()
     drive.setMotorModel(0,0)
-    servo.setServoAngle('0', 90)         # Set servo 0 to 90 degrees
-    servo.setServoAngle('1', 140)        # Set servo 1 to 140 degrees
+    #servo.setServoAngle('0', 90)         # Set servo 0 to 90 degrees
+    #servo.setServoAngle('1', 140)        # Set servo 1 to 140 degrees
     print("\nEnd of program")          # Print an end message
 
 

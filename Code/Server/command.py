2cf56c807c23ce8d10e9fadc65f63829b1be2fe9
jjyuen2 <jjyuen2@illinois.edu>
2025-03-01 12:24:01 -0600

    testing reverse

--- Code/Server/command.py
+++ Code/Server/command.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         RaiseArm()
         Drive(-1000, -1000)
         PinchOut()
-        Drive(-1000, -1000)
+        Drive(-2000, 0)
 
         for i in range(len(leftSpeeds)):
             Drive(-leftSpeeds[i], -rightSpeeds[i])

aa46acf1d06c4eea7c6facc03ac9d65927d96ce0
jjyuen2 <jjyuen2@illinois.edu>
2025-03-01 09:37:24 -0600

    stopall command

--- Code/Server/command.py
+++ Code/Server/command.py
@@ -10,47 +10,33 @@ class Command:
 def test_Drive():
     from motor import tankMotor              # Import the tankMotor class from the motor module
     import time                              # Import the time module for sleep functionality
-    print("Running command")
-    try: 
-        drive = tankMotor()
-        drive.setMotorModel(2000, 0) 
-        print("The car should be moving right")
-        time.sleep(2)
-        drive.setMotorModel(0, 2000)
-        time.sleep(2)
-        print("The car should be moving left")
-    except KeyboardInterrupt:
-        drive.setMotorModel(0,0)
-        print("End")
+    drive = tankMotor()
+    drive.setMotorModel(2000, 0) 
+    print("The car should be moving right")
+    time.sleep(2)
+    drive.setMotorModel(0, 2000)
+    time.sleep(2)
+    print("The car should be moving left")
 
 def Pinch():
     from servo import Servo            # Import the Servo class from the servo module
     import time                        # Import the time module for sleep functionality
     print('Pinching Servo')  # Print a start message
     servo = Servo()                    # Initialize the Servo instance
-    try:
-        for i in range(90, 150, 1):
-            servo.setServoAngle('0', i)  # Set servo 0 to angle i
-            time.sleep(0.01)           # Wait for 0.01 seconds
-    except KeyboardInterrupt:              # Handle keyboard interrupt (Ctrl+C)
-        servo.setServoAngle('0', 90)         # Set servo 0 to 90 degrees
-        servo.setServoAngle('1', 140)        # Set servo 1 to 140 degrees
-        print("\nEnd of program")          # Print an end message
+    for i in range(90, 150, 1):
+        servo.setServoAngle('0', i)  # Set servo 0 to angle i
+        time.sleep(0.01)           # Wait for 0.01 seconds
     
 def DropArm():
     from servo import Servo            # Import the Servo class from the servo module
     import time                        # Import the time module for sleep functionality
     print('Pinching Servo')  # Print a start message
     servo = Servo()                    # Initialize the Servo instance
-    try:
-        for i in range(140, 90, -1):
-            servo.setServoAngle('1', i)  # Set servo 1 to angle i
-            time.sleep(0.01)           # Wait for 0.01 seconds
-    except KeyboardInterrupt:              # Handle keyboard interrupt (Ctrl+C)
-        servo.setServoAngle('0', 90)         # Set servo 0 to 90 degrees
-        servo.setServoAngle('1', 140)        # Set servo 1 to 140 degrees
-        print("\nEnd of program")          # Print an end message
-
+    
+    for i in range(140, 90, -1):
+        servo.setServoAngle('1', i)  # Set servo 1 to angle i
+        time.sleep(0.01)           # Wait for 0.01 seconds
+   
 def StopAll():
     from servo import Servo            # Import the Servo class from the servo module
     from motor import tankMotor              # Import the tankMotor class from the motor module
@@ -62,14 +48,14 @@ def StopAll():
     print("\nEnd of program")          # Print an end message
 
 
-
+End = 0
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 2:
         print("Parameter error: Please assign the device")       # Print an error message if no device is specified
         exit()                                                   # Exit the program
     if sys.argv[1] == 'Command' or sys.argv[1] == 'command':
-        End = 0
+        print("Running Command")
         while End != 0:
             test_Drive()                                         # Drive
             DropArm()

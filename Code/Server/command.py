e02d1230934263568efc133c26bd9dce7e6d1fde
mmonish276 <mmonish276@gmail.com>
2025-03-02 00:29:14 -0600

    v20

--- Code/Server/command.py
+++ Code/Server/command.py
@@ -1,9 +1,14 @@
 from motor import tankMotor              # Import the tankMotor class from the motor module
 from servo import Servo            # Import the Servo class from the servo module
 import time                              # Import the time module for sleep functionality
+import cv2                    #openCV vision code
 
 leftSpeeds = []
 rightSpeeds = []
+center_error = 100
+dist_error = 0
+center_threshold = 5
+distance_threshold = 50
 
 class Command:
     def __init__(self):
@@ -13,6 +18,10 @@ class Command:
         self.CMD_ACTION = "CMD_ACTION"
         self.CMD_SONIC = "CMD_SONIC"
         self.CMD_MODE ="CMD_MODE"
+
+
+
+
 
 
 
@@ -63,9 +72,26 @@ def StopAll():
     servo.setServoAngle('1', 140)        # Set servo 1 to 140 degrees
     print("\nEnd of program")          # Print an end message
 
-def Capture_Distance:
-    
+def check_error():
+    #center_error = center_error - 1
+    #dist_error = dist_error - 1
+    x = 0
 
+def pulse_turn(direction="right", duration=0.2, speed=2000):
+    pwm_motor= tankMotor()
+    """Send a short pulse to turn the car."""
+    if direction == "right":
+        pwm_motor.setMotorModel(speed, -speed)  # Left motor forward, right motor backward (turn right)
+    else:
+        pwm_motor.setMotorModel(-speed, speed)  # Left motor backward, right motor forward (turn left)
+        
+    time.sleep(duration)  # Run for the specified duration
+    pwm_motor.setMotorModel(0, 0)  # Stop motors after pulse
+
+
+    
+        
+    
 
 if __name__ == '__main__':
     import sys
@@ -76,29 +102,33 @@ if __name__ == '__main__':
         print("Running Command")
         Start()
     try:
-        #while 1:
-        DropArm()
-        PinchIn()
-        RaiseArm()
-        Drive(-1000, -1000)
-        Drive(-1000, 1000)
-        PinchOut()
+        while True:
+            if (abs(center_error) > center_threshold):
+                if center_error > 0:
+                    Drive(1000, -1000)
+                    center_error = center_error - 1
+                    print(center_error)
+                    #check_error()
+                else:
+                    Drive(-1000, 1000)
+                    #check_error()
+            DropArm()
+            PinchIn()
+            RaiseArm()
+            PinchOut()
+            pulse_turn("right", 0.1, 2000)
 
 
-        time.sleep(1)
-
-        for i in range(len(leftSpeeds) - 1, -1, -1):
-            Drive(-leftSpeeds[i], -rightSpeeds[i])
             time.sleep(1)
+
+            for i in range(len(leftSpeeds) - 1, -1, -1):
+                Drive(-leftSpeeds[i], -rightSpeeds[i])
+                time.sleep(1)
 
 
             
     except KeyboardInterrupt:
         StopAll()
-        
-       
-
-          
 
 
 

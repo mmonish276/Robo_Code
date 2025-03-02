4a08c269e515fc3116d57551e1f686d33e29b83f
jjyuen2 <jjyuen2@illinois.edu>
2025-03-02 04:14:07 -0600

    PLEASE PLEASE PLEASE

--- Code/Server/command.py
+++ Code/Server/command.py
@@ -165,17 +165,17 @@ if __name__ == '__main__':
 
             while (abs(center_distance) > center_threshold):
                 if center_distance > 0:
-                    pulse_turn("left", 0.25, 2000)
+                    pulse_turn("left", 0.1, 1000)
                     get_center()
                     print(center_distance)
                     #check_error()
                 elif center_distance:
-                    pulse_turn("right", 0.25, 2000)
+                    pulse_turn("right", 0.1, 1000)
                     get_center()
                     print(center_distance)
                     #check_error()
 
-            Drive(2000,2000)
+            Drive(-2000,2000)
             # DropArm()
             # PinchIn()
             # RaiseArm()
